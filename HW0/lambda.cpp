f34a001942da16b999fb88dcb6c78240f37654ca
AmZu1212 <zuabiamir123@gmail.com>
2024-06-08 13:04:18 +0300

    first upload of lambda.cpp, and some base files

---
+++ HW0/lambda.cpp
@@ -0,0 +1,95 @@
+#include <lambda.h>
+#include <iostream>
+#include <algorithm>
+
+
+// gets an int, returns its absolute value.
+int abs(int num)
+{
+    if (num < 0)
+    {
+        return num * -1;
+    }
+    else
+    {
+        return num;
+    }
+}
+
+// this function initializes the by_value, by_ref, cmp_lambda
+// lambda pointers.
+void init_globals()
+{
+    /*
+     *  Excercise 1, a+b:
+     *  Define a lambda function which gets a vector of integers by value
+     *  and returns a new vector that contains only the even numbers from
+     *  the input vector. Assign that lambda function to the extern global
+     *  variable "by_value".
+     */
+    by_value = [](std::vector<int> numVector)
+    {
+        std::vector<int> evenNumsOnly;
+        for (int num : numVector)
+        {
+            if (num % 2 == 0)
+            {
+                evenNumsOnly.push_back(num);
+            }
+        }
+        return evenNumsOnly;
+    };
+
+    /*
+     *  Excercise 1, c+d:
+     *  Define a lambda function which gets a vector of integers by reference
+     *  and returns a new vector that contains only the even numbers from
+     *  the input vector. Assign that lambda function to the extern global
+     *  variable "by_ref".
+     */
+    by_ref = [](std::vector<int> &numVector)
+    {
+        std::vector<int> evenNumsOnly;
+        for (int num : numVector)
+        {
+            if (num % 2 == 0)
+            {
+                evenNumsOnly.push_back(num);
+            }
+        }
+        return evenNumsOnly;
+    };
+
+    /*
+     *  Excercise 1, e+f:
+     *  Write a lambda function that compares two integers by their absolute values.
+     *  This lambda function gets a global integer counter and increment it per each
+     *  compare operation. Assign that lambda function to the extern global variable
+     *  "cmp_lambda".
+     */
+    cmp_lambda = [](int &a, int &b) -> bool
+    {
+        return abs(a) < abs(b);
+    };
+}
+
+
+/*
+*  Excercise 2, g+h:
+*  Write the ascending_sort function to sort a vector of integers in 
+*  ascending order of their absolute values, using the C++ STL sort. 
+*  The ascending_sort() should return the sorted vector should print 
+*  the total amount of comparison operations applied during the sort.
+*/
+std::vector<int> ascending_sort(std::vector<int> &vec)
+{
+    // initialize lambda pointers
+    init_globals();
+    // sort from beggining to end using out cmp_lambda
+    std::sort(vec.begin(), vec.end(), cmp_lambda);
+    // print the amount of comparisons
+    std::cout << g_counter << std::endl;
+    return vec;
+}
+
+
